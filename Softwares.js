34c4a4761f58371b9fac5d8d8a19a9386a0f802f
Pro-File <team.logicart@gmail.com>
2020-12-31 19:56:05 +0500

    Implemented Softwares Page Successfully

---
+++ Softwares.js
@@ -0,0 +1,22 @@
+$('.carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplayTimeout:2000,
+   
+    responsive: {
+        0:{
+            items:1,
+          
+        },
+        630:{
+            items:2,
+         
+
+        },
+        1000:{
+            items:3,
+         
+
+        }
+    }
+})
